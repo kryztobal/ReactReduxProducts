7298f84e82da71f651aca1bbe0314fe3f800e578
cristobal muñoz <cmunoz@pzotec503.transbolivar.com.ve>
2017-12-26 20:07:01 -0400

    v6

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import store from './store';
+import { Provider } from 'react-redux';
+import { loadProducts } from './actionsCreator';
+
+store.dispatch(loadProducts());
+
+
+ReactDOM.render(
+  <Provider store={store}>
+  	<App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/store.js
@@ -0,0 +1,31 @@
+import { createStore, applyMiddleware } from 'redux';
+
+const reducer = (state, action) => {
+	if (action.type === "REPLACE_PRODUCTS"){
+		return {
+			...state,
+			products: action.products
+		}
+	}else if(action.type === "ADD_TO_CART") {
+		return {
+			...state,
+			cart: state.cart.concat(action.product)
+		};
+	} else if (action.type === "REMOVE_TO_CART") {
+		
+		return {
+			...state,
+			cart: state.cart.filter(product => product.id !== action.product.id)
+		}
+	} 
+	return state;
+}
+
+const logger = store => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  return result
+}
+
+export default createStore(reducer, { cart : [] }, applyMiddleware(logger));
